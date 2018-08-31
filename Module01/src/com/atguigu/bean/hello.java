5d478421c9c3e8e27a0a605f652d0a94e6f434c4
bbqianshen <178995064@qq.com>
2018-08-31 09:21:05 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/hello.java
@@ -0,0 +1,24 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+
+/**
+ * @author Jason.Qian
+ * @date 2018/8/29 - 8:31
+ */
+public class hello {
+
+
+
+    public static void main(String args[]) {
+
+
+        System.out.println("123");
+
+        ArrayList<Object> ss = new ArrayList<>();
+
+        ArrayList list = null;
+
+//        synchronized ()
+    }
+}
